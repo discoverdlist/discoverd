6fc54606ebf58d6a096e720d1d502e350bcaab30
ADG <2007andredoe@gmail.com>
2024-02-18 16:41:39 -0300

    using nextjs link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-async-client-component */
 'use client';
+import Link from "next/link";
 import Image from "next/image";
 import Script from "next/script";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,9 +13,9 @@ export default function Navbar() {
         return (
             <nav>
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 mt-4 relative">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <Image className="navbarlogo" src="/assets/logo.png" width="64" height="64" alt="Discord List" />
-                    </a>
+                    </Link>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative">
                         <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown">
                             <Image className="avatar" src={session.user.image} width="40" height="40" alt={session.user.name} />
@@ -22,16 +23,16 @@ export default function Navbar() {
                         <div id="dropdownDefault" data-dropdown-content className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg mt-2 shadow w-44 absolute top-full left-0 dark:bg-gray-700">
                             <ul>
                                 <li>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Perfil</a>
+                                    <Link href="#" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Perfil</Link>
                                 </li>
                                 <li>
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Configurações</a>
+                                    <Link href="#" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Configurações</Link>
                                 </li>
                                 <li>
-                                    <a href="/bots/add" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Adicionar bot</a>
+                                    <Link href="/bots/add" className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Adicionar bot</Link>
                                 </li>
                                 <li>
-                                    <a href="#" onClick={() => signOut()} className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Sair</a>
+                                    <Link href="#" onClick={() => signOut()} className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Sair</Link>
                                 </li>
                             </ul>
                         </div>
@@ -52,13 +53,13 @@ export default function Navbar() {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Início</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Início</Link>
                             </li>
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Explorar</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Explorar</Link>
                             </li>
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Parceiros</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Parceiros</Link>
                             </li>
                         </ul>
                     </div>
@@ -69,9 +70,9 @@ export default function Navbar() {
         return (
             <nav>
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 mt-4">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <Image className="navbarlogo" src="/assets/logo.png" width="64" height="64" alt="Discord List" />
-                    </a>
+                    </Link>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button type="button" onClick={() => signIn('discord')} className="loginbutton text-white focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-4 py-2 text-center"><FontAwesomeIcon className="dcicon" width="20" height="20" icon={faDiscord}/>&nbsp;Entrar com Discord</button>
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-sticky" aria-expanded="false">
@@ -84,13 +85,13 @@ export default function Navbar() {
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Início</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Início</Link>
                             </li>
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Explorar</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Explorar</Link>
                             </li>
                             <li>
-                                <a href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Parceiros</a>
+                                <Link href="#" className="md:text-white block py-2 px-3 text-white rounded md:hover:bg-transparent md:p-0 md:dark:hover:bg-transparent dark:border-gray-700">Parceiros</Link>
                             </li>
                         </ul>
                     </div>
