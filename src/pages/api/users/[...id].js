e8ab6ba8efec001e7e7ece773df39e063e912b6d
ADG <2007andredoe@gmail.com>
2024-02-18 17:20:52 -0300

    update: id.js

--- src/pages/api/users/[...id].js
+++ src/pages/api/users/[...id].js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
         }
     }).catch(() => res.status(500).json({ message: "Internal server error" }))
     if (response.status === 200) {
-        response.data.message = "Hello from the API"
         res.status(200).json(response.data)
     }
 }
