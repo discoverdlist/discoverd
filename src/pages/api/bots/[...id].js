847e0705756cea4bded837994c2f5bca26b37d1b
ADG <2007andredoe@gmail.com>
2024-02-20 11:02:26 -0300

    fix api

--- src/pages/api/bots/[...id].js
+++ src/pages/api/bots/[...id].js
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
                 }
             }
         }
-        response.apiKey = 'REDACTED'
         res.status(200).json(response.data)
     }
 }
