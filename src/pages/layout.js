54cba050ac37daa022fad83dfd4f19cc107a14e6
ADG <2007andredoe@gmail.com>
2024-02-20 09:50:54 -0300

    added  metadata

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,11 +1,17 @@
-"use client"
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Discoverd",
+  description: "Explore os melhores bots brasileiros para seu servidor!",
+  image: "/assets/logo.png",
+  url: "https://list.andrepaiva.dev",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={inter.className}>
           {children}
       </body>
