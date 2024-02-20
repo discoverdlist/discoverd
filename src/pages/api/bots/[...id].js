15cda53cf21c2414df643e74828b62fac038d507
ADG <2007andredoe@gmail.com>
2024-02-20 11:00:13 -0300

    remove api key from public endpoint

--- src/pages/api/bots/[...id].js
+++ src/pages/api/bots/[...id].js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
                 }
             }
         }
+        response.apiKey = 'REDACTED'
         res.status(200).json(response.data)
     }
 }
