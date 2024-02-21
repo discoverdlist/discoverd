55a9a19ad5cdde7d425bebed5dc216067dad52ee
ADG <2007andredoe@gmail.com>
2024-02-21 09:41:20 -0300

    added metadata

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,8 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { SessionProvider } from "next-auth/react"
-import './styles/globals.css'
+import './styles/globals.css';
+import Head from 'next/head';
 config.autoAddCss = false
 
 export default function App({
@@ -10,6 +11,12 @@ export default function App({
 }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Discoverd</title>
+        <meta name="description" content="Encontre os melhores bots brasileiros para seu servidor no Discord! Tudo isso de maneira fácil e rápida e em um só lugar, venha para a Discoverd!" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content="discord, discord bots, discord bot list, discord list, discord bot, discord botlist, discordlist, discord bot list website, discord botlist website, discordlist website, discord bot list site, discord botlist site, discordlist site, discord bot list web, discord botlist web, discordlist web, brasil, brazil, brasileiro, br" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
